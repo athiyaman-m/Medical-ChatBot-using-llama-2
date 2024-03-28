2568749d03897fc0fe393dc7c141ba69bc664157
athiyaman-m <athiyamanpro@gmail.com>
2024-03-28 06:00:04 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Athiyaman M',
+    author_email= 'athiyamanpro@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
